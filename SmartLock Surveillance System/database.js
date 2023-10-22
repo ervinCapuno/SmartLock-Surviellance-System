87127ad373a5e3aeb9b4a6443fd4338cae00b07e
Ervin Capuno <21-00082@g.batstate-u.edu.ph>
2023-10-23 06:37:43 +0800

    Created the UI Dashboards and fix the smaller screen in the dashboard

---
+++ SmartLock Surveillance System/database.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAnBPT65Kbl2NhTe_CadDJCjnruM7vg4ss",
+  authDomain: "smartlock-surviellance-system.firebaseapp.com",
+  projectId: "smartlock-surviellance-system",
+  storageBucket: "smartlock-surviellance-system.appspot.com",
+  messagingSenderId: "516963910372",
+  appId: "1:516963910372:web:c2102c0d0ce0d2125e22f1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+// https://smartlock-surviellance-system-default-rtdb.asia-southeast1.firebasedatabase.app/
